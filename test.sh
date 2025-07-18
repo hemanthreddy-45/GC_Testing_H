e2c1562e461ae9dbf48d9504ad5c7f8ce8a87182
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2025-07-18 18:33:15 +0530

    KMS KEY

--- test.sh
+++ test.sh
@@ -1,15 +1,1 @@
-﻿This
-is
-some
-text
-info
----
-test-8
-test-9
-// JavaScript version
 const kmsKeyId = "abcd1234-56ef-78gh-90ij-klmnopqrstuv";  // hardcoded KMS key
-
-function encrypt(data) {
-    console.log("Using KMS Key:", kmsKeyId);
-    return `encrypted(${data})`;
-}
