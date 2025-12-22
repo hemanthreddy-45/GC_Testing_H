907962a1ca59b64492bc0ff048b8eb3391d7dbbe
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2025-12-22 15:38:02 +0530

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -2,3 +2,6 @@ token = "P$5aRt#2oN!9lM@8as"
 token = "p328D219@!U9SAD"
 token = "p328D219@!U9SADal2j132"
 token = "p328D219@!U9SADal2j"
+api_key_name  = "auw2p-s3-presigned-key
+encryption_kms_key  = "1234ab56-78cd-9ef0-12h3-12345678i901"
+kms_master_key_id    = "1234ab56-78cd-9ef0-12h3-12345678i901"
